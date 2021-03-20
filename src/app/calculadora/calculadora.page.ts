8a0b08cca9eaf0d4a18144158dfb0855f8d847a1
Daniel Giraldo <qwertydaniel4@gmail.com>
2021-03-20 11:10:14 -0500

    Calculadora

--- src/app/calculadora/calculadora.page.ts
+++ src/app/calculadora/calculadora.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { isNumber } from 'util';
 
 @Component({
   selector: 'app-calculadora',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculadora.page.scss'],
 })
 export class CalculadoraPage implements OnInit {
+  
 
   constructor() { }
 
   ngOnInit() {
+  }
+  value = '0';
+  readyForNewInput = true;
+
+  numberGroups = [
+    [7,8,9, 'x'],
+    [4,5,6, '-'],
+    [1,2,3, '+'],
+    [0,'.','%','=']
+  ];
+
+  onButtonPress(num){
+    console.log(num);
+
+    if (isNumber(num))
+      console.log('Es un numero');
+      if (this.readyForNewInput)
+        this.value =  '' + num;
+      else
+      this.value +=  '' + num;
   }
 
 }
